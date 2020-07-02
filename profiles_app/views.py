c7f69e381e86c9561f540b714f294c09d1785591
Sai Ganesh S <saiyuva050929@gmail.com>
2020-07-02 12:49:25 +0530

    completed get, post, put, patch methods

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_app import serializers
 # Create your views here.
 
 
 class HellowApiView(APIView):
-
+    serializer_class = serializers.HelloSerializer
     def get(self, request, format=None):
         an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
@@ -14,3 +16,25 @@ class HellowApiView(APIView):
             'Is mapped manually to URLs',
         ]
         return Response({'message': 'hello','an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hellow {name} '
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
